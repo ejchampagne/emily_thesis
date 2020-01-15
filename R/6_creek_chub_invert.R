c7c36d712160f99899bcb678c8f520c8a94074dc
Emily <emilyjchampagne@gmail.com>
2020-01-15 14:50:09 -0500

    added cc ratio and ter predator regressions against coupling and TP

--- R/6_creek_chub_invert.R
+++ R/6_creek_chub_invert.R
@@ -21,20 +21,81 @@ terinv = terinv %>% mutate(predatory = p_aranae + p_hymenoptera + p_opiliones + 
 abun <- right_join(cc_count, aqinv)
 abun <- right_join(abun, pcascores)
 abun <- right_join(abun, pcadata)
+abun <- right_join(abun, terinv, by = "sitecode")
 
-si <- right_join(si, cc_count)
-si <- right_join(si, aqinv, by = "sitecode")
+abun %>% filter(totalcount.x < 100)
 
-mod <- lm(log10(abun$cc_n/abun$edibles) ~ abun$log_ag_250)
-summary(mod)
+plot(((abun$edibles+1)/abun$totalcount.x) ~ abun$log_ag_250)
 
-plot(log10(abun$cc_n/abun$edibles) ~ abun$log_ag_250, ylab="log10 creek chub:edible invertebrate abundance", xlab = "log10 250 m radial buffer % ag", pch = 19)
-abline(mod)
+plot(log10(abun$cc_n+1) ~ abun$log_ag_250)
 
-abun1 <- abun %>% filter(!sitename == "Hawkcliff")
+summary(lm(log10(abun$cc_n+1) ~ abun$log_ag_250))
 
-mod <- lm(log10(abun1$cc_n/abun1$edibles) ~ abun1$log_ag_250)
-summary(mod)
+# calculate ratio of cc to edible inverts
+abun$ratio_cc_edible <- abun$cc_n/abun$edibles
+abun$ratio_cc_totalaq <- abun$cc_n/abun$totalcount.x
 
-plot(log10(abun1$cc_n/abun1$edibles) ~ abun1$log_ag_250)
-abline(mod)
+plot(log10(abun$ratio_cc_edible+1) ~ abun$log_ag_250)
+summary(lm(log10(abun$ratio_cc_edible+1) ~ abun$log_ag_250))
+
+# make some plots
+mod_ag <- lm(ratio_cc_edible ~ log_ag_250, data = abun)
+summary(mod_ag)
+
+plot(log10(ratio_cc_edible) ~ log_ag_250, data = abun, pch = 19)
+abline(mod_ag)
+
+mod_buf <- lm(log10(ratio_cc_edible) ~ log_buf_width, data = abun)
+summary(mod_buf)
+
+plot(log10(ratio_cc_edible) ~ log_buf_width, data = abun, pch = 19)
+abline(mod_buf)
+
+cc %>% filter(species == "creek chub") %>% 
+  group_by(sitecode) %>%
+  summarise(count = n()) %>%
+  right_join(pcadata) %>%
+  ggplot(aes(x = log_ag_250, y = log10(count))) + geom_point() + geom_smooth(se = F, method = "lm")
+
+cc %>% filter(species == "creek chub") %>% 
+  group_by(sitecode) %>%
+  summarise(mean_w = mean(weight, na.rm = T)) %>%
+  right_join(pcadata) %>%
+  ggplot(aes(x = log_ag_250, y = mean_w)) + geom_point() 
+
+abun <- right_join(abun, si, by = "sitecode")
+
+# model for terrestrial energy
+hist(plogis(abun$ter_energy_pp))
+mod_prop_ter <- lm(plogis(ter_energy_pp) ~ log_ag_250.x + ratio_cc_edible + forkmm, data = abun)
+summary(mod_prop_ter)
+
+# model for trophic position
+hist(abun$tp_2_pp)
+mod_tp <- lm(tp_2_pp ~ log_ag_250.x + predatory + forkmm, data = abun)
+summary(mod_tp)
+mod_tp <- update(mod_tp, .~. -forkmm)
+summary(mod_tp)
+mod_tp <- update(mod_tp, .~. -predatory)
+summary(mod_tp)
+
+
+# plots
+# terrestrial energy
+avPlots(mod_prop_ter,"forkmm", xlab="partial forklength (mm)", ylab="logit proportion terrestrial energy", 
+        grid = F, id = F, pch = 19, col.lines = "black", bty ="l", ylim = c(-0.18, 0.18))
+
+avPlots(mod_prop_ter,"log_ag_250.x", xlab="log10 partial local % agriculture (250 m buffer)", ylab="logit proportion terrestrial energy", 
+        grid = F, id = F, pch = 19, col.lines = "black", bty ="l", ylim = c(-0.18, 0.18))
+
+avPlots(mod_prop_ter,"ratio_cc_edible", xlab="partial creek chub:edible aquatic invertebrate ratio", ylab="logit proportion terrestrial energy", 
+        grid = F, id = F, pch = 19, col.lines = "black", bty ="l", ylim = c(-0.18, 0.18))
+
+
+# trophic position
+avPlots(mod_tp,"log_ag_250.x",  ylab="trophic position", xlab="log10 partial local % agriculture (250 m buffer)",
+        grid = F, id = F, pch = 19, col.lines = "black", bty ="l", ylim = c(-5.5, 2))
+
+avPlots(mod_tp,"predatory",  ylab="trophic position", xlab="partial proportion terrestrial predatory insects",
+        grid = F, id = F, pch = 19, col.lines = "black", bty ="l", ylim = c(-5.5, 2))
+
